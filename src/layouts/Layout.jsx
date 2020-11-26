aa998d3fb9c5f1cf2d2dd2d2aa7f8d95fdf218a6
steodec <paul.tedesco@ynov.com>
2020-11-26 11:35:29 +0100

    feat: Add calendar without design

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -14,9 +14,10 @@ import Home from "../pages/home";
 import Login from "../pages/login";
 import Register from "../pages/register";
 import GettingStarted from "../pages/gettingStarted";
+import Calendar from "../pages/calendar";
 
-const Layout = ({ children, ...props }) => {
-  const { pathname } = useLocation();
+const Layout = ({children, ...props}) => {
+  const {pathname} = useLocation();
   const fullPages = ["/login", "/register", "/getting-started"];
 
   const isFullPage = (page) => {
@@ -28,28 +29,31 @@ const Layout = ({ children, ...props }) => {
   const cssClasses = isFullPage(pathname) ? "full" : null;
 
   return (
-    <>
-      <LocaleProvider>
-        {!isFullPage(pathname) ? <Header /> : null}
-        <Container className={cssClasses}>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/login">
-              <Login />
-            </Route>
-            <Route exact path="/register">
-              <Register />
-            </Route>
-            <Route exact path="/getting-started">
-              <GettingStarted />
-            </Route>
-          </Switch>
-        </Container>
-        {!isFullPage(pathname) ? <Footer /> : null}
-      </LocaleProvider>
-    </>
+   <>
+     <LocaleProvider>
+       {!isFullPage(pathname) ? <Header/> : null}
+       <Container className={cssClasses}>
+         <Switch>
+           <Route exact path="/">
+             <Home/>
+           </Route>
+           <Route exact path="/login">
+             <Login/>
+           </Route>
+           <Route exact path="/register">
+             <Register/>
+           </Route>
+           <Route exact path="/getting-started">
+             <GettingStarted/>
+           </Route>
+           <Route exact path="/calendar">
+             <Calendar/>
+           </Route>
+         </Switch>
+       </Container>
+       {!isFullPage(pathname) ? <Footer/> : null}
+     </LocaleProvider>
+   </>
   );
 };
 
--- src/pages/calendar.jsx
+++ src/pages/calendar.jsx
@@ -1,15 +1,73 @@
 import FullCalendar, { formatDate } from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from ' ';
+import interactionPlugin from '@fullcalendar/interaction';
+import React, { useState } from 'react';
+import { isLoggedIn } from "../utils/auth";
+import { Redirect } from "react-router-dom";
+import { createEventId } from "../utils/envent-utils";
+import { Button, Title } from "../components";
 
 const Calendar = () => {
+  const [weekendsVisible, setWeekendsVisible] = useState(true);
+  const [currentEvents, setCurrentEvents] = useState([]);
   if (!isLoggedIn()) return <Redirect to={"/getting-started"}/>;
+
+  const handleWeekendsToggle = () => {
+    setWeekendsVisible(!weekendsVisible);
+  };
+
+  const handleDateSelect = (selectInfo) => {
+    let title = prompt('Please enter a new title for your event');
+    let calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect();
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      });
+    }
+  };
+
+  const handleEvents = (events) => {
+    setCurrentEvents(events);
+  };
+
+  const renderSidebar = () => {
+    return (
+     <div className='demo-app-sidebar'>
+       <div className='demo-app-sidebar-section'>
+         <label>
+           <input
+            type='checkbox'
+            checked={weekendsVisible}
+            onChange={handleWeekendsToggle}
+           />
+           toggle weekends
+         </label>
+       </div>
+       <div className='demo-app-sidebar-section'>
+         <h2>All Events ({currentEvents.length})</h2>
+         <ul>
+           {currentEvents.map(renderSidebarEvent)}
+         </ul>
+       </div>
+     </div>
+    );
+  };
+
   return (
    <>
      <Title level={2}>Calendar</Title>
+     {renderSidebar()}
      <FullCalendar
       plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      customButtons={Button}
       headerToolbar={{
         left: 'prev,next today',
         center: 'title',
@@ -21,14 +79,30 @@ const Calendar = () => {
       selectMirror={true}
       dayMaxEvents={true}
       weekends={weekendsVisible}
-      initialEvents={INITIAL_EVENTS}
       select={handleDateSelect}
       eventContent={renderEventContent}
-      eventClick={handleEventClick}
       eventsSet={handleEvents}
      />
    </>
   );
 };
+
+function renderEventContent(eventInfo) {
+  return (
+   <>
+     <b>{eventInfo.timeText}</b>
+     <i>{eventInfo.event.title}</i>
+   </>
+  );
+}
+
+function renderSidebarEvent(event) {
+  return (
+   <li key={event.id}>
+     <b>{formatDate(event.start, {year: 'numeric', month: 'short', day: 'numeric'})}</b>
+     <i>{event.title}</i>
+   </li>
+  );
+}
 
 export default Calendar;
