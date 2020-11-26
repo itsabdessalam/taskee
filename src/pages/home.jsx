e8e3fedcd4b98c757326f3104d2da62390151124
Floran-mtte <floran.maitte@gmail.com>
2020-11-26 16:22:58 +0100

    feat: add push notification

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,14 @@
 import { FormattedMessage } from "react-intl";
+import NotificationService from "../services/NotificationService";
 
 import { Title } from "../components";
 
 const Home = () => {
+
+  const testNotification = () => {
+    NotificationService.send('test', 'test message', 'https://taskee.playgrnd.dev');
+  };
+
   return (
     <>
       <Title level={2}>Home</Title>
@@ -13,8 +19,10 @@ const Home = () => {
         }}
       />
       {/* only for test purposes */}
+      <button onClick={testNotification}></button>
     </>
   );
+
 };
 
 export default Home;
