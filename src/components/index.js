3551f2ae00fec4c82d15383fb85630289b1741a6
Abdessalam Benharira <belmehelabdessalam.benharira@ynov.com>
2020-11-26 15:41:21 +0100

    feat: add editor.js component

--- src/components/index.js
+++ src/components/index.js
@@ -14,6 +14,7 @@ import LoginForm from "./LoginForm";
 import ProtectedRoute from "./ProtectedRoute";
 import RegisterForm from "./RegisterForm";
 import CreateNoteForm from "./CreateNoteForm";
+import Editor from "./Editor";
 
 export {
   Button,
@@ -31,5 +32,6 @@ export {
   CreateNoteForm,
   Select,
   Title,
-  LocaleSelector
+  LocaleSelector,
+  Editor
 };
---
+++ src/pages/editorTest.jsx
@@ -0,0 +1,12 @@
+import { Title, Editor } from "../components";
+
+const EditorTest = () => {
+  return (
+    <>
+      <Title level={2}>Editor</Title>
+      <Editor />
+    </>
+  );
+};
+
+export default EditorTest;
