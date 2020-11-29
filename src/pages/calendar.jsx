2f9ed02d013eba6059a3bd8ec1fca97770f3df67
Naomi <naomi.paulmin@gmail.com>
2020-11-29 18:15:13 +0100

    lint: run linter

--- src/pages/calendar.jsx
+++ src/pages/calendar.jsx
@@ -19,71 +19,71 @@ const Calendar = () => {
 
   const renderSidebar = () => {
     return (
-     <div className="demo-app-sidebar">
-       <div className="demo-app-sidebar-section">
-         <label>
-           <input
-            type="checkbox"
-            checked={weekendsVisible}
-            onChange={handleWeekendsToggle}
-           />
-           toggle weekends
-         </label>
-       </div>
-       <div className="demo-app-sidebar-section">
-         <h2>All Events ({currentEvents.length})</h2>
-         <ul>{currentEvents.map(renderSidebarEvent)}</ul>
-       </div>
-     </div>
+      <div className="demo-app-sidebar">
+        <div className="demo-app-sidebar-section">
+          <label>
+            <input
+              type="checkbox"
+              checked={weekendsVisible}
+              onChange={handleWeekendsToggle}
+            />
+            toggle weekends
+          </label>
+        </div>
+        <div className="demo-app-sidebar-section">
+          <h2>All Events ({currentEvents.length})</h2>
+          <ul>{currentEvents.map(renderSidebarEvent)}</ul>
+        </div>
+      </div>
     );
   };
 
   return (
-   <>
-     <Title level={2}>Calendar</Title>
-     {renderSidebar()}
-     <FullCalendar
-      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-      customButtons={Button}
-      headerToolbar={{
-        left: "prev,next today",
-        center: "title",
-        right: "dayGridMonth,timeGridWeek,timeGridDay"
-      }}
-      initialView="dayGridMonth"
-      editable={true}
-      selectable={true}
-      selectMirror={true}
-      dayMaxEvents={true}
-      weekends={weekendsVisible}
-      eventContent={renderEventContent}
-      eventsSet={handleEvents}
-     />
-   </>
+    <>
+      <Title level={2}>Calendar</Title>
+      {renderSidebar()}
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        customButtons={Button}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay"
+        }}
+        initialView="dayGridMonth"
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        dayMaxEvents={true}
+        weekends={weekendsVisible}
+        eventContent={renderEventContent}
+        eventsSet={handleEvents}
+      />
+    </>
   );
 };
 
 function renderEventContent(eventInfo) {
   return (
-   <>
-     <b>{eventInfo.timeText}</b>
-     <i>{eventInfo.event.title}</i>
-   </>
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
   );
 }
 
 function renderSidebarEvent(event) {
   return (
-   <li key={event.id}>
-     <b>
-       {formatDate(event.start, {
-         year: "numeric",
-         month: "short",
-         day: "numeric"
-       })}
-     </b>
-     <i>{event.title}</i>
-   </li>
+    <li key={event.id}>
+      <b>
+        {formatDate(event.start, {
+          year: "numeric",
+          month: "short",
+          day: "numeric"
+        })}
+      </b>
+      <i>{event.title}</i>
+    </li>
   );
 }
 
