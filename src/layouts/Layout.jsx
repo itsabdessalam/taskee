dc6b346554d508a3c1466824deab3b5574034306
steodec <paul.tedesco@ynov.com>
2020-11-29 15:56:54 +0100

    feat: offline mode

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,12 +1,7 @@
+/* eslint-disable no-unused-vars */
 import { Route, Switch, Redirect } from "react-router-dom";
 import { Container, Navigation } from "../layouts";
-import { ProtectedRoute } from "../components";
-/* eslint-disable no-unused-vars */
-import { Route, Switch, useLocation } from "react-router-dom";
-import { Container, Footer, Header } from "../layouts";
-import { LocaleProvider } from "../context/Locale";
 import { ProtectedRoute, Offline } from "../components";
-import Home from "../pages/home";
 import Login from "../pages/login";
 import Register from "../pages/register";
 import GettingStarted from "../pages/gettingStarted";
@@ -17,39 +12,37 @@ import Calendar from "../pages/calendar";
 const Layout = ({ children, ...props }) => {
   return (
     <>
-      <LocaleProvider>
-        {!isFullPage(pathname) ? <Header /> : null}
-        <Offline />
-        <Container className={cssClasses}>
-          <Switch>
-            <ProtectedRoute exact path="/">
-              <Home />
-            </ProtectedRoute>
-            <Route exact path="/login">
-              <Login />
-            </Route>
-            <Route exact path="/register">
-              <Register />
-            </Route>
-            <Route exact path="/getting-started">
-              <GettingStarted />
-            </Route>
-            <ProtectedRoute exact path="/notes">
-              <Notes />
-            </ProtectedRoute>
-            <ProtectedRoute exact path="/notes/new">
-              <CreateNote />
-            </ProtectedRoute>
-            <ProtectedRoute exact path="/notes/:id">
-              <Notes />
-            </ProtectedRoute>
-            <Route exact path="/editor-test">
-              <EditorTest />
-            </Route>
-          </Switch>
-        </Container>
-        {!isFullPage(pathname) ? <Footer /> : null}
-      </LocaleProvider>
+      <Navigation />
+      <Offline />
+      <Container>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/notes" />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/getting-started">
+            <GettingStarted />
+          </Route>
+          <ProtectedRoute exact path="/notes">
+            <Notes />
+          </ProtectedRoute>
+          <ProtectedRoute exact path="/notes/:id">
+            <Notes />
+          </ProtectedRoute>
+          <ProtectedRoute exact path="/settings">
+            <Settings />
+          </ProtectedRoute>
+          <ProtectedRoute exact path="/calendar">
+            <Calendar />
+          </ProtectedRoute>
+          <Redirect to="/notes" /> {/* please keep it last */}
+        </Switch>
+      </Container>
     </>
   );
 };
